1d7a93cecb0172441dbd39eb083874e17c0a8a35
David Hein <david-hein196@web.de>
2021-09-19 20:37:30 +0200

    Add comments Remove line breaks

--- automated_script.py
+++ automated_script.py
@@ -11,24 +11,26 @@ parser.add_argument('user', type=str, help='username for login')
 parser.add_argument('password', type=str, help='password for login', default=False)
 args = parser.parse_args()
 
+# Init connection
 wurzelBot = main.initWurzelBot()
 wurzelBot.launchBot(args.server, args.user, args.password)
 
+# Havest
 wurzelBot.harvestAllGarden()
 
-
+# Plant plants
 planted = -1
-
 while planted != 0:
     lowest = wurzelBot.getLowestStockEntry()
     if lowest == 'Your stock is empty': break
     print(lowest + ' anpflanzen...')
     planted = wurzelBot.growPlantsInGardens(lowest)
 
+# Water plants
 time.sleep(3)
 wurzelBot.waterPlantsInAllGardens()
 
-#Deinitialisierung des Bots
+# Close connection
 wurzelBot.exitBot()
 
 
