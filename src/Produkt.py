b99f9ea1e7acd474ef2ce415da68fbf93ef2df77
David Hein <david-hein196@web.de>
2022-01-18 17:47:41 +0100

    Rename isProductPlantable() to isPlantable()

--- src/Produkt.py
+++ src/Produkt.py
@@ -39,7 +39,7 @@ class Product():
     def getPriceNPC(self):
         return self.__priceNPC
     
-    def isProductPlantable(self):
+    def isPlantable(self):
         return self.__isPlantable
         
     def setPriceNPC(self, price):
