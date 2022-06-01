78473fab0b0b1e80b3e231d794437ac04e8e942f
xRuffKez <p.scholing.93@gmail.com>
2022-06-01 18:56:10 +0200

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -12,9 +12,43 @@ tmpdir="${wbdir}/tmp"
 datadir="${wbdir}/data"
 
 # Check distribution
+echo "Checking distribution..."
 
-# Check requirements
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+    echo "$NAME $VERSION_ID is supported."
+elif type lsb_release >/dev/null 2>&1; then
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+    echo "$(lsb_release -si) $(lsb_release -sr) is supported."
+elif [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+    echo "$DISTRIB_ID $DISTRIB_RELEASE is supported."
+elif [ -f /etc/debian_version ]; then
+    echo "Your OS is outdated and NOT supported! INSTALLATION FAILED!"
+    exit
+elif [ -f /etc/SuSe-release ]; then
+    echo "Your OS is outdated and NOT supported! INSTALLATION FAILED!"
+    exit
+elif [ -f /etc/redhat-release ]; then
+    echo "Your OS is outdated and NOT supported! INSTALLATION FAILED!"
+    exit
+else
+    echo "Your OS is NOT supported (yet)!  INSTALLATION FAILED!"
+    exit
+fi
+
 
 # Check root and deny if present
+if [ "$(id -u)" = "0" ]; then
+   echo "DO NOT RUN THIS SCRIPT AS ROOT!"
+   exit
+fi
+
+# Check requirements
 
 # Check for update if present else install
