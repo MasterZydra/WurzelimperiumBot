9bd7cd2d92ce312c9b65047c7464368a2847b99a
xRuffKez <p.scholing.93@gmail.com>
2022-06-01 19:07:39 +0200

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+# Script written by xRuffKez for WurzelimperiumBot by MrFlamez & MasterZydra
+# PS: Tot den Maulwürfen!
+
+# Meister Proper :D
+printf "\033c"
 
 # vars
 rootdir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 selfname=$(basename "$(readlink -f "${BASH_SOURCE[0]}")")
-sessionname=$(echo "${selfname}" | cut -f1 -d".")
+sessionname=$(printf "${selfname}" | cut -f1 -d".")
 wbdir="${rootdir}/wurzelbot"
 logdir="${rootdir}/log"
 wblogdir="${logdir}/logs"
@@ -12,40 +17,40 @@ tmpdir="${wbdir}/tmp"
 datadir="${wbdir}/data"
 
 # Check distribution
-echo "Checking distribution..."
+printf "Checking distribution...\n"
 
 if [ -f /etc/os-release ]; then
     . /etc/os-release
     OS=$NAME
     VER=$VERSION_ID
-    echo "$NAME $VERSION_ID is supported."
+    printf "$NAME $VERSION_ID is supported.\n"
 elif type lsb_release >/dev/null 2>&1; then
     OS=$(lsb_release -si)
     VER=$(lsb_release -sr)
-    echo "$(lsb_release -si) $(lsb_release -sr) is supported."
+    printf "$(lsb_release -si) $(lsb_release -sr) is supported.\n"
 elif [ -f /etc/lsb-release ]; then
     . /etc/lsb-release
     OS=$DISTRIB_ID
     VER=$DISTRIB_RELEASE
-    echo "$DISTRIB_ID $DISTRIB_RELEASE is supported."
+    printf "$DISTRIB_ID $DISTRIB_RELEASE is supported."
 elif [ -f /etc/debian_version ]; then
-    echo "Your OS is outdated and NOT supported! INSTALLATION FAILED!"
+    printf "Your OS is outdated and NOT supported!\nINSTALLATION FAILED!\n"
     exit
 elif [ -f /etc/SuSe-release ]; then
-    echo "Your OS is outdated and NOT supported! INSTALLATION FAILED!"
+    printf "Your OS is outdated and NOT supported!\nINSTALLATION FAILED!\n"
     exit
 elif [ -f /etc/redhat-release ]; then
-    echo "Your OS is outdated and NOT supported! INSTALLATION FAILED!"
+    printf "Your OS is outdated and NOT supported!\nINSTALLATION FAILED!\n"
     exit
 else
-    echo "Your OS is NOT supported (yet)!  INSTALLATION FAILED!"
+    printf "Your OS is NOT supported (yet)!\nINSTALLATION FAILED!\n"
     exit
 fi
 
 
 # Check root and deny if present
 if [ "$(id -u)" = "0" ]; then
-   echo "DO NOT RUN THIS SCRIPT AS ROOT!"
+   printf "DO NOT RUN THIS SCRIPT AS ROOT!\nINSTALLATION FAILED!\n"
    exit
 fi
 
