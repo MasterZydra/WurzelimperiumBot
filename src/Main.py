619149f90fd4bcf931f417d706332a305c6742da
xRuffKez <92758042+xRuffKez@users.noreply.github.com>
2022-06-05 04:07:51 +0200

    Added Log Directory and Log File splitting by datetime

--- src/Main.py
+++ src/Main.py
@@ -7,11 +7,19 @@ Created on 21.03.2017
 '''
 from src.WurzelBot import WurzelBot
 import logging
+import os
+import time
+
+#vars
+logtime = time.strftime("%d-%m-%y-%H.%M.%S")
+
+if not os.path.exists('logs'):
+    os.makedirs('logs')
 
 def initWurzelBot():
     return WurzelBot()
 
 def logger():
-    logging.basicConfig(filename='wurzelbot.log', level=logging.DEBUG, format='%(asctime)s - %(message)s')
+    logging.basicConfig(filename='logs/wurzelbot '+logtime+'.log', level=logging.DEBUG, format='%(asctime)s - %(message)s')
 
 #TODO: Konstruktor prüfen, evtl um Accountdaten erweitern
