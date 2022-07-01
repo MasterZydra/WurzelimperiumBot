cfacd16ddc0bb715ce8524a28954b8cbe120f6ec
xRuffKez <92758042+xRuffKez@users.noreply.github.com>
2022-07-02 00:57:35 +0200

    Revert "Merge branch 'master' of https://github.com/MasterZydra/WurzelimperiumBot"
    This reverts commit 1c273fbb63d135e9964b9b9c38b4f3c71070b4cf, reversing
    changes made to 05c15fccaf9d9b57dfe41d22b29eef50e3559452.

--- src/WurzelBot.py
+++ src/WurzelBot.py
@@ -351,7 +351,7 @@ class WurzelBot(object):
         for productID in self.storage.getOrderedStockList():
             if not self.productData.getProductByID(productID).isPlant() or \
                 not self.productData.getProductByID(productID).isPlantable() or \
-                self.productData.getSX() != 1 or self.productData.getSY() != 0:
+                not self.productData.getProductByID(productID).getName() in self.productData.getListOfSingleFieldPlants():
                 continue
 
             currentStock = self.storage.getStockByProductID(productID)
