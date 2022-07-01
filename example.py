87df047e392b30fbd9ecd38647dadf3bbc521d5d
xRuffKez <92758042+xRuffKez@users.noreply.github.com>
2022-07-01 18:37:10 +0200

    Added language flag

--- example.py
+++ example.py
@@ -17,6 +17,8 @@ log = True
 user = ''
 pw = ''
 server = 46
+# lang = de/en/ru
+lang = 'de'
 
 # Init logger
 if log == True:
@@ -24,7 +26,7 @@ if log == True:
 
 #Login und Initialisierung des Bots
 wurzelBot = WurzelBot()
-wurzelBot.launchBot(server, user, pw)
+wurzelBot.launchBot(server, user, pw, lang)
 
 #TODO: Aktionen definieren
 #Beispiel: Alles ernten, in allen Gärten Kürbis anbauen und alles gießen
