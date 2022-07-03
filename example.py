7359497f5d75ac9181b04c414884d99208cf5ee5
Igor Ivashkov <gosha_iv@mail.ua>
2022-07-03 16:09:28 +0300

    Add lang variable to example.py

--- example.py
+++ example.py
@@ -13,29 +13,26 @@ Alle Stellen die angepasst werden müssen sind mit TODO gekennzeichnet.
 # Logging? Set to True to enable or False to disable logging.
 log = True
 
-#TODO: Login Daten eintragen
+# TODO: Login Daten eintragen
 user = ''
 pw = ''
 server = 46
+lang = ''
 
 # Init logger
-if log == True:
+if log:
     logger.logger()
 
-#Login und Initialisierung des Bots
+# Login und Initialisierung des Bots
 wurzelBot = WurzelBot()
-wurzelBot.launchBot(server, user, pw)
+wurzelBot.launchBot(server, user, pw, lang)
 
-#TODO: Aktionen definieren
-#Beispiel: Alles ernten, in allen Gärten Kürbis anbauen und alles gießen
+# TODO: Aktionen definieren
+# Beispiel: Alles ernten, in allen Gärten Kürbis anbauen und alles gießen
 wurzelBot.harvestAllGarden()
-wurzelBot.growPlantsInGardens('Salat', 2) # Nur 2 Pflanzen
-wurzelBot.growPlantsInGardens('Kürbis') # So viele Pflanzen wie möglich
+wurzelBot.growPlantsInGardens('Salat', 2)  # Nur 2 Pflanzen
+wurzelBot.growPlantsInGardens('Kürbis')  # So viele Pflanzen wie möglich
 wurzelBot.waterPlantsInAllGardens()
 
-#Deinitialisierung des Bots
+# Deinitialisierung des Bots
 wurzelBot.exitBot()
-
-
-
-
