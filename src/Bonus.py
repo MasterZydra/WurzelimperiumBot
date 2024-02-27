cc981c87a9c72eee0a76bcc706ae1d1ff30720c0
xRuffKez <92758042+xRuffKez@users.noreply.github.com>
2024-02-27 11:46:53 +0100

    Optimize Bonus.py

--- src/Bonus.py
+++ src/Bonus.py
@@ -7,9 +7,16 @@ class Bonus:
 
     def getDailyLoginBonus(self):
         bonus_data = self.__httpConn.readUserDataFromServer(data_type="dailyloginbonus")['dailyloginbonus']
-        for day, bonus in bonus_data['data']['rewards'].items():
-            if 'done' not in bonus:
-                if any(_ in bonus for _ in ('money', 'products')):
-                    self.__httpConn.getDailyLoginBonus(day)
-                    time.sleep(3)
+        bonuses_to_claim = [(day, bonus) for day, bonus in bonus_data['data']['rewards'].items()
+                            if 'done' not in bonus and any(_ in bonus for _ in ('money', 'products'))]
+
+        self.__claim_login_bonuses(bonuses_to_claim)
+
+    def __claim_login_bonuses(self, bonuses_to_claim):
+        for day, _ in bonuses_to_claim:
+            self.__httpConn.getDailyLoginBonus(day)
+            
+        sleep_time = max(0, len(bonuses_to_claim) - 1) * 3
+        time.sleep(sleep_time)
+
                     
