f9710f2c84d5e39422e5a8720fb85b9265fd94de
xRuffKez <92758042+xRuffKez@users.noreply.github.com>
2024-02-27 04:40:38 +0100

    Update wimpbot_systemd_install.sh for current stable debian/ubuntu systems

--- wimpbot_systemd_install.sh
+++ wimpbot_systemd_install.sh
@@ -24,8 +24,6 @@
 
 # vars
 rootdir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-grp="$(id -g)"
-usr="$(whoami)"
 wbdir="${rootdir}/wurzelbot"
 confdir="${wbdir}/conf"
 tmpdir="${wbdir}/tmp"
@@ -34,65 +32,78 @@ gituser="MasterZydra"
 gitrepo="WurzelimperiumBot"
 gitbranch="master"
 
-printf "\033c" # Meister Proper :D
+# Distribution und Version erkennen
+distribution=$(lsb_release -si)
+version=$(lsb_release -sr)
 
-# Check root and deny if present
+# Unterstützte Distributionen und Versionen definieren
+supported_distributions=("Debian" "Ubuntu")
+supported_versions=("11" "12" "20.04" "20.10" "22.04" "22.10")
+
+# Überprüfen, ob die Distribution und Version unterstützt werden
+if [[ ! " ${supported_distributions[@]} " =~ " ${distribution} " ]] || \
+   [[ ! " ${supported_versions[@]} " =~ " ${version} " ]]; then
+    echo "Dieses Skript ist für ${distribution} ${version} nicht kompatibel."
+    exit 1
+fi
+
+# Paketmanager und -befehle je nach Distribution festlegen
+case $distribution in
+    "Debian")
+        install_command="apt-get install -y"
+        ;;
+    "Ubuntu")
+        install_command="apt install -y"
+        ;;
+esac
+
+# Prüfen, ob das Skript als Root ausgeführt wird
 if [ "$(id -u)" = "0" ]; then
-   printf "DO NOT RUN THIS SCRIPT AS ROOT!\nINSTALLATION FAILED!\n"
-   exit
+    echo "Führen Sie dieses Skript nicht als Root aus!"
+    exit 1
 fi
 
-printf "Checking sudo access... "
-su=$(sudo -n uptime 2>&1 | grep -c "load")
-if [ "${su}" -gt 1 ]; then
-    printf "FAIL\nYou have no sudo access, which is required for installing WurzelimperiumBot!\nINSTALLATION FAILED!\n"
-    exit
-else
-    printf "OK\n"
+# Prüfen, ob sudo-Zugriff vorhanden ist
+if ! sudo -n true 2>/dev/null; then
+    echo "Sie haben keinen sudo-Zugriff, der für die Installation erforderlich ist!"
+    exit 1
 fi
 
-# Check distribution
-printf "Checking distribution...\n"
+# Pakete installieren
+echo "Installiere erforderliche Pakete..."
+sudo $install_command python3 python3-pip git || exit 1
 
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    printf "%s is supported.\n" "$NAME $VERSION_ID"
-elif type lsb_release >/dev/null 2>&1; then
-    printf "%s is supported.\n" "$(lsb_release -si) $(lsb_release -sr)"
-elif [ -f /etc/lsb-release ]; then
-    . /etc/lsb-release
-    printf "%s is supported.\n" "$DISTRIB_ID $DISTRIB_RELEASE"
-else
-    printf "Your OS is NOT supported!\nINSTALLATION FAILED!\n"
-    exit
+# Temporäre Dateien löschen und Verzeichnisse erstellen
+echo "Temporäre Dateien löschen und Verzeichnisse erstellen..."
+rm -rf "$tmpdir" "$datadir"
+mkdir -p "$wbdir" "$datadir" "$tmpdir" "$confdir"
+
+# Konfigurationsdatei erstellen, wenn sie nicht existiert
+echo "Konfigurationsdatei erstellen..."
+acc_conf="$confdir/acc.conf"
+if [ ! -f "$acc_conf" ]; then
+    touch "$acc_conf"
+    cat <<EOF > "$acc_conf"
+# SERVERNR USERNAME PASSWORT
+# KEINE RAUTE (#) davor setzen!!! Beispiel: 12 Hildegart camping2016
+EOF
 fi
 
+# WurzelimperiumBot-Repository klonen
+echo "WurzelimperiumBot-Repository klonen..."
+git clone https://github.com/"${gituser}"/"${gitrepo}".git --depth 1 --branch="${gitbranch}" "$tmpdir" || exit 1
+mv "$tmpdir"/* "$datadir" && rm -rf "$tmpdir"
 
-# Check requirements
-printf "Checking requirements...\n"
-sudo apt update && sudo apt -y upgrade && sudo apt install -y python3 pip git
+# Python-Anforderungen installieren
+echo "Python-Anforderungen installieren..."
+pip3 install --upgrade pip || exit 1
+pip3 install -r "${datadir}/requirements.txt" || exit 1
 
-# Clone from master
-printf "Cleaning temp files.\n"
-rm -rf "${tmpdir}" && rm -rf "${datadir}"
-printf "Checking directories and creating them if needed.\n"
-[ ! -d "${wbdir}" ] && mkdir "${wbdir}"
-[ ! -d "${datadir}" ] && mkdir "${datadir}"
-[ ! -d "${tmpdir}" ] && mkdir "${tmpdir}"
-[ ! -d "${confdir}" ] && mkdir "${confdir}"
-[ ! -f "${confdir}"/acc.conf ] && touch "${confdir}"/acc.conf && echo "# SERVERNR USERNAME PASSWORT" >> "${confdir}"/acc.conf && echo "# KEINE RAUTE (#) davor setzen!!! Beispiel: 12 Hildegart camping2016" >> "${confdir}"/acc.conf 
-
-printf "Retrieving files for WurzelimperiumBot.\n"
-git clone https://github.com/"${gituser}"/"${gitrepo}".git --depth 1 --branch="${gitbranch}" "${tmpdir}"
-mv "${tmpdir}"/* "${datadir}" && rm -rf "${tmpdir}"
-
-# Install py requirements with pip
-pip install --upgrade pip && pip install -r "${datadir}"/requirements.txt
-
-# Creating worker
-[ ! -f "${rootdir}"/worker.sh ] && touch "${rootdir}"/worker.sh && chmod +x "${rootdir}"/worker.sh
-
-cat <<EOT >> "${rootdir}"/worker.sh
+# Worker-Skript erstellen
+echo "Worker-Skript erstellen..."
+worker_script="$rootdir/worker.sh"
+if [ ! -f "$worker_script" ]; then
+    cat <<'EOF' > "$worker_script"
 #!/bin/bash
 # Script written by xRuffKez for WurzelimperiumBot by MrFlamez & MasterZydra
 
@@ -103,37 +114,39 @@ cat <<EOT >> "${rootdir}"/worker.sh
 timer=60
 ##
 
-
-while true
-do 
-
-    conf='${confdir}/acc.conf'
+while true; do
+    conf="$confdir/acc.conf"
     while read -r line; do
-        [[ "\$line" =~ ^#.*$ ]] && continue
-        python3 ${datadir}/automated_script.py \${line}
-    done < "\$conf"
-    sleep "\$timer"
+        [[ "$line" =~ ^#.*$ ]] && continue
+        python3 "${datadir}/automated_script.py" "$line"
+    done < "$conf"
+    sleep "$timer"
 done
-EOT
+EOF
+    chmod +x "$worker_script"
+fi
 
-# Creating systemd service
-sudo systemctl stop wimpbot && sudo systemctl disable wimpbot && sudo rm /lib/systemd/system/wimpbot.service && sudo touch /lib/systemd/system/wimpbot.service
-
-cat <<EOT >> "${rootdir}"/wimpbot.service
+# systemd-Dienst erstellen und starten
+echo "systemd-Dienst erstellen und starten..."
+cat <<EOF | sudo tee "/lib/systemd/system/wimpbot.service" >/dev/null
 [Unit]
 Description=WurzelimperiumBot
 
 [Service]
 Type=simple
-User=${usr}
-Group=${grp}
-WorkingDirectory=${rootdir}
-ExecStart=${rootdir}/worker.sh
+User=$(id -un)
+Group=$(id -gn)
+WorkingDirectory=$rootdir
+ExecStart=$worker_script
 
 [Install]
 WantedBy=multi-user.target
-EOT
-sudo mv "${rootdir}"/wimpbot.service /lib/systemd/system/wimpbot.service && sudo systemctl daemon-reload  && sudo systemctl enable wimpbot && sudo systemctl start wimpbot
-printf "\033c" # Meister Proper :D
-sudo systemctl status wimpbot
-printf "\n\n\nInstallation finished!\n\nPlease configure acc.conf in the conf directory!\n\n"
+EOF
+
+sudo systemctl daemon-reload || exit 1
+sudo systemctl enable wimpbot || exit 1
+sudo systemctl start wimpbot || exit 1
+
+# Installation abschließen
+echo "Installation abgeschlossen!"
+echo "Bitte konfigurieren Sie die Datei acc.conf im Verzeichnis conf."
