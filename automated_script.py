246f29dabbba0999b0568660a3564215e5c4ca29
xRuffKez <92758042+xRuffKez@users.noreply.github.com>
2024-02-28 20:22:32 +0100

    Update automated_script.py
    - Prevent execution through parent scripts.
    - Better handling and exiting of Bot, on exceptions.
    - Reorganisation of statements while keep readability of code.

--- automated_script.py
+++ automated_script.py
@@ -1,74 +1,80 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
+import time
+import i18n
 import src.Logger as logger
 from src.WurzelBot import WurzelBot
-import time, i18n, argparse
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('server', type=int, help='server number')
+    parser.add_argument('user', type=str, help='username for login')
+    parser.add_argument('password', type=str, help='password for login', default=False)
+    parser.add_argument('-p', '--portal', help="If -p or --portal Argument is passed, Portal Account Login will be used.", action='store_true', default=False, required=False, dest="portalacc")
+    parser.add_argument('-l', '--log', help="If -l or --log Argument is passed, logging will be enabled.", action='store_true', default=False, required=False, dest="log")
+    parser.add_argument('lang', help="Set Language and Region for the Game and Bot", type=str, nargs='?', default=None, const='en')
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument('server', type=int, help='server number')
-parser.add_argument('user', type=str, help='username for login')
-parser.add_argument('password', type=str, help='password for login', default=False)
-parser.add_argument('-p', '--portal', help="If -p or --portal Argument is passed, Portal Account Login will be used.", action='store_true', default=False, required=False, dest="portalacc")
-parser.add_argument('-l', '--log', help="If -l or --log Argument is passed, logging will be enabled.", action='store_true', default=False, required=False, dest="log")
-parser.add_argument('lang', help="Set Language and Region for the Game and Bot", type=str, nargs='?', default=None, const='en')
-args = parser.parse_args()
+    i18n.load_path.append('lang')
+    i18n.set('locale', args.lang)
+    i18n.set('fallback', 'en')
 
-i18n.load_path.append('lang')
-i18n.set('locale', args.lang)
-i18n.set('fallback', 'en')
+    if args.log:
+        logger.logger()
 
-if args.log:
-    logger.logger()
+    # Init connection
+    # BG- Създаване на връзка
+    wurzelBot = WurzelBot()
+    succ = wurzelBot.launchBot(args.server, args.user, args.password, args.lang, args.portalacc)
+    if not succ:
+        exit(-1)
 
-# Init connection
-#BG- Създаване на връзка
-wurzelBot = WurzelBot()
-succ = wurzelBot.launchBot(args.server, args.user, args.password, args.lang, args.portalacc)
-if succ != True:
-    exit(-1)
+    try:
+        # Remove weed
+        # BG-Премахване на плевели
+        print(i18n.t('wimpb.remove_weed_from_all_gardens'))
+        wurzelBot.removeWeedInAllGardens()
 
-# Remove weed
-#BG-Премахване на плевели
-print(i18n.t('wimpb.remove_weed_from_all_gardens'))
-wurzelBot.removeWeedInAllGardens()
+        # Harvest
+        # BG-Жътва
+        wurzelBot.harvestAllGarden()
 
-# Harvest
-#BG-Жътва
-wurzelBot.harvestAllGarden()
+        # Plant plants
+        # BG-Посаждане на растения
+        while wurzelBot.hasEmptyFields():
+            lowest = wurzelBot.getLowestVegetableStockEntry()
+            if lowest == 'Your stock is empty':
+                break
+            print(i18n.t('wimpb.grow_plant_X', plant=lowest))
+            planted = wurzelBot.growVegetablesInGardens(lowest)
+            if planted == 0:
+                lowest_single = wurzelBot.getLowestSingleVegetableStockEntry()
+                if lowest_single == 'Your stock is empty':
+                    break
+                print(i18n.t('wimpb.grow_plant_X', plant=lowestSingle))
+                wurzelBot.growVegetablesInGardens(lowestSingle)
 
-# Plant plants
-#BG-Посаждане на растения
-planted = -1
-plantedSingle = -1
-while wurzelBot.hasEmptyFields() and planted != 0 and plantedSingle != 0:
-    lowest = wurzelBot.getLowestVegetableStockEntry()
-    if lowest == 'Your stock is empty': break
-    print(i18n.t('wimpb.grow_plant_X', plant=lowest))
-    planted = wurzelBot.growVegetablesInGardens(lowest)
-    # If no more "lowest plants" can be grown, try to grow single field plants
-    #BG-Ако няма растения с малък брой, посаждане на еденични такива.
-    if planted == 0:
-        lowestSingle = wurzelBot.getLowestSingleVegetableStockEntry()
-        if lowestSingle == 'Your stock is empty': break
-        print(i18n.t('wimpb.grow_plant_X', plant=lowestSingle))
-        plantedSingle = wurzelBot.growVegetablesInGardens(lowestSingle)
+        # Water plants
+        # BG-Поливане на растенията
+        time.sleep(3)
+        print(i18n.t('wimpb.watering_all_plants'))
+        wurzelBot.waterPlantsInAllGardens()
 
-# Water plants
-#BG-Поливане на растенията
-time.sleep(3)
-print(i18n.t('wimpb.watering_all_plants'))
-wurzelBot.waterPlantsInAllGardens()
-# Claim Daily
-#BG-Събиране на дневният бонус
-print(i18n.t('wimpb.claim_bonus'))
-wurzelBot.getDailyLoginBonus()
-# Process Wimp Customers in Gardens
-#BG-Изпълни нуждите на Wimps в градините
-print(i18n.t('wimpb.process_wimps'))
-wurzelBot.sellWimpsProducts(0,0)
+        # Claim Daily
+        # BG-Събиране на дневният бонус
+        print(i18n.t('wimpb.claim_bonus'))
+        wurzelBot.getDailyLoginBonus()
 
-# Close connection
-#BG-Затваряне на връзката
-wurzelBot.exitBot()
+        # Process Wimp Customers in Gardens
+        # BG-Изпълни нуждите на Wimps в градините
+        print(i18n.t('wimpb.process_wimps'))
+        wurzelBot.sellWimpsProducts(0, 0)
+    finally:
+        # Close connection
+        # BG-Затваряне на връзката
+        wurzelBot.exitBot()
+
+if __name__ == "__main__":
+    main()
