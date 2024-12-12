0d9f09be49fa4bbccbb58b165b9871da6a2a84aa
David Hein <44842054+MasterZydra@users.noreply.github.com>
2024-12-12 17:01:57 +0100

    Added Products.py with constants for each product
    #110

---
+++ src/product/Products.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+COINS = 0
+CHERRY = 1 # DE: Kirsche
+SALAD = 2  # DE: Salat
+STRAWBERRY = 3 # DE: Erdbeere
+APPLE = 4 # DE: Apfel
+TOMATO = 5 # DE: Tomate
+CARROT = 6 # DE: Karotte
+EGGPLANT = 7 # DE: Aubergine
+BLACKBERRY = 8 # DE: Brombeere
+ONION = 9 # DE: Zwiebel
+
+RASPBERRY = 10 # DE: Himbeere
+CURRANT = 11 # DE: Johannisbeere
+CUCUMBER = 12 # DE: Gurke
+PLUM = 13 # DE: Pflaume
+RADISH = 14 # DE: Radieschen
+SWEET_BELL_PEPPER = 15 # DE: Paprika
+COURGETTE = 16 # DE: Zucchini
+MIRABELLE_PLUM = 17 # DE: Mirabelle
+PUMPKIN = 18 # DE: Kürbis
+WALNUT = 19 # DE: Walnuss
+
+ASPARAGUS = 20 # DE: Spargel
+PEAR = 21 # DE: Birne
+POTATO = 22 # DE: Kartoffel
+WELL_1 = 23 # DE: Brunnen 1
+FOUNTAIN_2 = 24 # DE: Brunnen 2
+DECORATIVE_STONES = 25 # DE: Dekosteine
+SIDEWALK_1 = 26 # DE: Gehweg 1
+SIDEWALK_2 = 27 # DE: Gehweg 2
+SIDEWALK_3 = 28 # DE: Gehweg 3
+PAVILION_1 = 29
+
+PAVILION_2 = 30
+PAVILION_3 = 31
+CAULIFLOWER = 32 # DE: Blumenkohl
+BROCCOLI = 33 # DE: Brokkoli
+BLUEBERRY = 34 # DE: Heidelbeere
+GARLIC = 35 # DE: Knoblauch
+SPINACH = 36 # DE: Spinat
+SITTING_AREA = 37 # DE: Sitzecke
+ZEN_GARDEN = 38 # DE: Zengarten
+POND = 39 # DE: Teich
+
+STONE_CIRCLE = 40 # DE: Steinkreis
+WEEDS = 41 # DE: Unkraut
+TREE_STUMP = 42 # DE: Baumstumpf
+STONE = 43 # DE: Stein
+GARDEN_GNOME = 44 # DE: Gartenzwerge
+MOLE = 45 # DE: Maulwurf
+SIDEWALK_4 = 46 # DE: Gehweg 4
+WOODEN_BALLS = 47 # DE: Holzkugeln
+SUNFLOWER = 48 # DE: Sonnenblume
+MARIGOLD = 49 # DE: Ringelblume
+
+ROSE = 50 # DE: Rose
+LILY = 51 # DE: Lilie
+CORNFLOWER = 52 # DE: Kornblume
+ORCHID = 53 # DE: Orchidee
+CROCUS = 54 # DE: Krokus
+OLIVE = 55
+FIREPLACE = 56 # DE: Feuerstelle
+SCARECROW = 57 # DE: Vogelscheuche
+GERBERA = 58
+LAVENDER = 59 # DE: Lavendel
+
+TULIP = 60 # DE: Tulpe
+RED_CABBAGE = 61 # DE: Rotkohl
+SANDPIT = 62 # DE: Sandkasten
+SLIDE = 63 # DE: Rutsche
+COFFEE = 64 # DE: Kaffee
+COCONUT = 65 # DE: Kokosnuss
+PALM_TREE = 66 # DE: Palme
+GRAPE = 67 # DE: Traube
+LEMON = 68 # DE: Zitrone
+MUSHROOM = 69 # DE: Champignon
+
+BASIL = 70 # DE: Basilikum
+ORANGE = 71 # DE: Orange
+COCOA = 72 # DE: Kakao
+
+BEAN = 181 # DE: Bohne
+
+FIELD_MINT = 225 # DE: Ackerminze
